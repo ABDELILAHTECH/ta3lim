cb1f773f476c223022e681097c784fb9d7db0371
ABDELILAHTECH <ABDELILAHTECH1@GMAIL.COM>
2025-09-27 13:30:19 +0100

    c

--- Frontend/src/Components/LevelsNav.jsx
+++ Frontend/src/Components/LevelsNav.jsx
@@ -13,7 +13,7 @@ export default function LevelsNav() {
                 {
                     levels.map((levelItem,index)=>{
                         return(
-                            <li key={index} onClick={() => toggleLevel(levelItem)} className={`text-lg text-center md:text-2xl lg:text-3xl  py-5 flex-1 cursor-pointer  border-b-4 border-primary   ${levelItem === level ? " border-slate-900 dark:border-white"  : ""} `}>{levelItem}</li>
+                            <li key={index} onClick={() => toggleLevel(levelItem)} className={`text-lg text-center md:text-2xl lg:text-3xl  py-2  flex-1 cursor-pointer  border-b-4 border-primary   ${levelItem === level ? " border-slate-900 dark:border-white"  : ""} `}>{levelItem}</li>
                         )
                     })
                 }
