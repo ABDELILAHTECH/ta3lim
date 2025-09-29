e67f4b174b65a8934333d68c8f91a38fb1f8a947
abdelilah <abdelilahtech1@gmail.com>
2025-09-29 09:10:05 +0100

    c

--- Frontend/src/Components/LevelsNav.jsx
+++ Frontend/src/Components/LevelsNav.jsx
@@ -8,12 +8,12 @@ export default function LevelsNav() {
 
 
   return (
-    <nav className='w-full bg-primary '>
+    <nav className='w-full lg:w-[70%]'>
             <ul className='flex justify-center'>
                 {
                     levels.map((levelItem,index)=>{
                         return(
-                            <li key={index} onClick={() => toggleLevel(levelItem)} className={`text-lg text-center md:text-2xl lg:text-3xl  py-2  flex-1 cursor-pointer  border-b-4 border-primary   ${levelItem === level ? " border-slate-900 dark:border-white"  : ""} `}>{levelItem}</li>
+                            <li key={index} onClick={() => toggleLevel(levelItem)} className={`text-lg text-center md:text-2xl py-2  flex-1 cursor-pointer ${levelItem === level ? "border-b-4 border-primary text-primary dark:border-"  : ""} `}>{levelItem}</li>
                         )
                     })
                 }
