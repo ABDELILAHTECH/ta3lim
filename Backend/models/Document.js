016ec70667c85e94a1e4f681a587472da346be39
abdelilah <abdelilahtech1@gmail.com>
2025-10-02 12:04:24 +0100

    c

--- Backend/models/Document.js
+++ Backend/models/Document.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const documentSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  type: { type: String, enum: ["الدروس", "التمارين", "الفروض","الامتحانات"], required: true },
+  type: { type: String, enum: ["الدروس", "التمارين", "الفروض", "المراقبة المستمرة", "الامتحانات"], required: true },
   subject: { type: String, required: true }, 
   class: { type: String, required: true },
   level:{ type: String ,enum: ["الابتدائي", "الإعدادي", "الثانوي"] , required: true},
